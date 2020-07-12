de048bfe1300c698d589f3420d2c2589800b7fc4
Alex <axo895@student.bham.ac.uk>
2020-07-12 19:45:06 +0100

    Added Blog Application

---
+++ Blog/tests.py
@@ -0,0 +1,7 @@
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+from django.urls import resolve
+from django.test import TestCase
+from Blog.views import PostListView
+
+# Create your tests here.
