513be89f5c5b21ea554baae46c4d2870a3f05f8d
Alex <axo895@student.bham.ac.uk>
2020-07-13 21:41:32 +0100

    Passed the test

--- CV/models.py
+++ CV/models.py
@@ -8,3 +8,11 @@ class About(models.Model):
 
     def __str__(self):
         return self.description
+
+class Experience(models.Model):
+    job_description = models.TextField(max_length=256)
+    job_start_date = models.DateField()
+    job_end_date = models.DateField()
+
+    def __str__(self):
+        return self.job_description
--- CV/tests.py
+++ CV/tests.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest
 from django.test import TestCase
-from CV.models import About
+from CV.models import About, Experience
 # Create your tests here.
 from CV.views import home_page
 
@@ -14,16 +14,17 @@ class tests(TestCase):
         about = About.objects.create(description="THis is a test")
         about.save()
         if About.objects.get(pk=1):
+            print(About.objects.get(pk=1))
             self.assertIsNotNone(About.objects.get(pk=1))
         else:
             self.fail("Didn't create the about model")
 
-
-
     def test_for_experience_model(self):
-        exp = Experience.objects.create(job_description="Testing the exp",job_start_date="2020-06-01",job_end_date="2020-09-01")
+        exp = Experience.objects.create(job_description="Testing the exp", job_start_date="2020-06-01",
+                                        job_end_date="2020-09-01")
         exp.save()
         if Experience.objects.get(pk=1):
+            print(Experience.objects.get(pk=1))
             self.assertIsNotNone(Experience.objects.get(pk=1))
         else:
             self.fail("Didn't create the experience model")
