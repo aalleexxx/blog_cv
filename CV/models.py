be91af8cae42d1ef135e1020833667bd39ca2913
Alex <axo895@student.bham.ac.uk>
2020-07-13 22:21:02 +0100

    Passed the tests

--- CV/models.py
+++ CV/models.py
@@ -9,10 +9,24 @@ class About(models.Model):
     def __str__(self):
         return self.description
 
+
 class Experience(models.Model):
-    job_description = models.TextField(max_length=256)
+    job_title = models.TextField(max_length=256)
+    job_description = models.TextField()
+    job_company = models.TextField(max_length=256)
     job_start_date = models.DateField()
     job_end_date = models.DateField()
 
     def __str__(self):
-        return self.job_description
+        return self.job_title
+
+
+class Education(models.Model):
+    certificate_title = models.TextField(max_length=512)
+    certificate_description = models.TextField()
+    certificate_start_date = models.DateField()
+    certificate_end_date = models.DateField()
+    certificate_institute = models.TextField(max_length=256)
+
+    def __str__(self):
+        return self.certificate_title
--- CV/tests.py
+++ CV/tests.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest
 from django.test import TestCase
-from CV.models import About, Experience
+from CV.models import About, Experience, Education
 # Create your tests here.
 from CV.views import home_page
 
@@ -21,18 +21,53 @@ class tests(TestCase):
 
     def test_for_experience_model(self):
         exp = Experience.objects.create(job_description="Testing the exp", job_start_date="2020-06-01",
-                                        job_end_date="2020-09-01")
+                                        job_end_date="2020-09-01", job_company="TESTINGG")
         exp.save()
         if Experience.objects.get(pk=1):
             print(Experience.objects.get(pk=1))
             self.assertIsNotNone(Experience.objects.get(pk=1))
+            self.assertEqual("Testing the exp", Experience.objects.get(pk=1).job_description)
         else:
             self.fail("Didn't create the experience model")
 
+    def test_for_experience_length_limit(self):
+        exp = Experience.objects.create(
+            job_title="nRpWUlgun7uFUWEUhsN1xwTdvwCeqqP6e35omB2xQ5z02vGyuG6LfYjXSAwUEP39VKTEamN5u6o60OgTUxzbLYu7WJYieviWbEdqc05Zdg4cjfhKJhzh538MRceQPtPAZcY37hIEl0C6RbtJxZEkwhKeQNzIsarSYyyzYjPr7fzzZoo0NoG23K2tBzfDtaIc8n9dGyRk3siMeFCy1drZUXOFdQhFWJ8obDIvHo5HatmHdcd80fnx4L48fdINnqHyzVTYgzQPTeOOCybNh7X0FxQAwbwYYEgeioY8DLjQMGNrodIyL8Jc5n2eUDgaMRL1Rolr7xehPGRomDEqSb4rcAxyflvI6QBz8GVIVYH5aidn2pSF9jAvkxR8Bv3D33mAHZesSfruXbU3HnThWtdjrZ29RBsXvRrofnIO5iRUnKQCGkBQcTWJikqf8TZKBfnsrpXCohSQH4DY0BCOZCnqeLdwXsQKRfDADJTESmljW8MlyvD3vI3Jm4iEBW98tOLPNUFURmk6ejiCtmgoF6izXNApAcwih68DqyvRqcnc9OQLnQlVcVfrbwp3zJ1oopsN9A7w6hAKOXKNxxDITEm3BShu8ufQaA3zO84SMS52ymliB21EBEuSU7TJB69uwA2mTrAJhgK6I2nThcjH1ZHaTR6peLzUgRYDEW2ENMFIitfOA5ypoAGyFhFn3tuO5wsNmf3vPjDMT488wzOuooQH1894wKL50lSRIvrHv6NrDgiXQrNYSwRvPIQAohhvQcMLDstEYf3GIauM2FQ2ZYhgSXBkQeIpGCsgHvgk2H7A1gzTmpTbUEFxa5EtJwzshkqWB2arjUEjaF8Xn5uqbX63Rih3eJmGxnD6LX2G2Y7vqG8rsNemlPnJnFie3x7hNAwrhJGZWqlevLQb9jrhqwg8kluF7So2ZAGOHUbd5MfWFrHRgTAdE2wbNjcNpBtNAB8lJqKT091kSB0ipP1idA2bPhBk0425xeq8ldsUO8r1",
+            job_description="Test",
+            job_start_date="2020-06-01", job_end_date="2022-09-01", job_company="Testing")
+        exp.save()
+        if Experience.objects.get(pk=1):
+            print("Should fail")
+            self.assertIsNotNone(Experience.objects.get(pk=1))
+            self.assertEqual(Experience.objects.get(pk=1).job_title,
+                             "nRpWUlgun7uFUWEUhsN1xwTdvwCeqqP6e35omB2xQ5z02vGyuG6LfYjXSAwUEP39VKTEamN5u6o60OgTUxzbLYu7WJYieviWbEdqc05Zdg4cjfhKJhzh538MRceQPtPAZcY37hIEl0C6RbtJxZEkwhKeQNzIsarSYyyzYjPr7fzzZoo0NoG23K2tBzfDtaIc8n9dGyRk3siMeFCy1drZUXOFdQhFWJ8obDIvHo5HatmHdcd80fnx4L48fdINnqHyzVTYgzQPTeOOCybNh7X0FxQAwbwYYEgeioY8DLjQMGNrodIyL8Jc5n2eUDgaMRL1Rolr7xehPGRomDEqSb4rcAxyflvI6QBz8GVIVYH5aidn2pSF9jAvkxR8Bv3D33mAHZesSfruXbU3HnThWtdjrZ29RBsXvRrofnIO5iRUnKQCGkBQcTWJikqf8TZKBfnsrpXCohSQH4DY0BCOZCnqeLdwXsQKRfDADJTESmljW8MlyvD3vI3Jm4iEBW98tOLPNUFURmk6ejiCtmgoF6izXNApAcwih68DqyvRqcnc9OQLnQlVcVfrbwp3zJ1oopsN9A7w6hAKOXKNxxDITEm3BShu8ufQaA3zO84SMS52ymliB21EBEuSU7TJB69uwA2mTrAJhgK6I2nThcjH1ZHaTR6peLzUgRYDEW2ENMFIitfOA5ypoAGyFhFn3tuO5wsNmf3vPjDMT488wzOuooQH1894wKL50lSRIvrHv6NrDgiXQrNYSwRvPIQAohhvQcMLDstEYf3GIauM2FQ2ZYhgSXBkQeIpGCsgHvgk2H7A1gzTmpTbUEFxa5EtJwzshkqWB2arjUEjaF8Xn5uqbX63Rih3eJmGxnD6LX2G2Y7vqG8rsNemlPnJnFie3x7hNAwrhJGZWqlevLQb9jrhqwg8kluF7So2ZAGOHUbd5MfWFrHRgTAdE2wbNjcNpBtNAB8lJqKT091kSB0ipP1idA2bPhBk0425xeq8ldsUO8r1")
+        else:
+            self.fail("Should fail for now")
+
     def test_for_education_model(self):
-        edu = Education.objects.create(certificate_description="Testing the education", certificate_start_date="2020-06-01",certification_end_date="2022-09-01")
+        edu = Education.objects.create(certificate_institute="TEST",
+                                       certificate_description="Testing the education",
+                                       certificate_start_date="2020-06-01", certificate_end_date="2022-09-01",
+                                       certificate_title="TESTING EDUC")
         edu.save()
         if Education.objects.get(pk=1):
             self.assertIsNotNone(Education.objects.get(pk=1))
+            self.assertEqual("TESTING EDUC", Education.objects.get(pk=1).certificate_title)
         else:
             self.fail("Didn't create the education model")
+
+    def test_for_education_length_limit(self):
+        # Benefits of TDD :)
+        edu = Education.objects.create(certificate_institute="TEST",
+                                       certificate_description="Testing the education",
+                                       certificate_start_date="2020-06-01", certificate_end_date="2022-09-01",
+                                       certificate_title="nRpWUlgun7uFUWEUhsN1xwTdvwCeqqP6e35omB2xQ5z02vGyuG6LfYjXSAwUEP39VKTEamN5u6o60OgTUxzbLYu7WJYieviWbEdqc05Zdg4cjfhKJhzh538MRceQPtPAZcY37hIEl0C6RbtJxZEkwhKeQNzIsarSYyyzYjPr7fzzZoo0NoG23K2tBzfDtaIc8n9dGyRk3siMeFCy1drZUXOFdQhFWJ8obDIvHo5HatmHdcd80fnx4L48fdINnqHyzVTYgzQPTeOOCybNh7X0FxQAwbwYYEgeioY8DLjQMGNrodIyL8Jc5n2eUDgaMRL1Rolr7xehPGRomDEqSb4rcAxyflvI6QBz8GVIVYH5aidn2pSF9jAvkxR8Bv3D33mAHZesSfruXbU3HnThWtdjrZ29RBsXvRrofnIO5iRUnKQCGkBQcTWJikqf8TZKBfnsrpXCohSQH4DY0BCOZCnqeLdwXsQKRfDADJTESmljW8MlyvD3vI3Jm4iEBW98tOLPNUFURmk6ejiCtmgoF6izXNApAcwih68DqyvRqcnc9OQLnQlVcVfrbwp3zJ1oopsN9A7w6hAKOXKNxxDITEm3BShu8ufQaA3zO84SMS52ymliB21EBEuSU7TJB69uwA2mTrAJhgK6I2nThcjH1ZHaTR6peLzUgRYDEW2ENMFIitfOA5ypoAGyFhFn3tuO5wsNmf3vPjDMT488wzOuooQH1894wKL50lSRIvrHv6NrDgiXQrNYSwRvPIQAohhvQcMLDstEYf3GIauM2FQ2ZYhgSXBkQeIpGCsgHvgk2H7A1gzTmpTbUEFxa5EtJwzshkqWB2arjUEjaF8Xn5uqbX63Rih3eJmGxnD6LX2G2Y7vqG8rsNemlPnJnFie3x7hNAwrhJGZWqlevLQb9jrhqwg8kluF7So2ZAGOHUbd5MfWFrHRgTAdE2wbNjcNpBtNAB8lJqKT091kSB0ipP1idA2bPhBk0425xeq8ldsUO8r1")
+        edu.save()
+        if Education.objects.get(pk=1):
+            print("Should fail")
+            print(Education.objects.get(pk=1).certificate_title)
+            self.assertIsNotNone(Education.objects.get(pk=1))
+            self.assertEqual(Education.objects.get(pk=1).certificate_title,
+                             "nRpWUlgun7uFUWEUhsN1xwTdvwCeqqP6e35omB2xQ5z02vGyuG6LfYjXSAwUEP39VKTEamN5u6o60OgTUxzbLYu7WJYieviWbEdqc05Zdg4cjfhKJhzh538MRceQPtPAZcY37hIEl0C6RbtJxZEkwhKeQNzIsarSYyyzYjPr7fzzZoo0NoG23K2tBzfDtaIc8n9dGyRk3siMeFCy1drZUXOFdQhFWJ8obDIvHo5HatmHdcd80fnx4L48fdINnqHyzVTYgzQPTeOOCybNh7X0FxQAwbwYYEgeioY8DLjQMGNrodIyL8Jc5n2eUDgaMRL1Rolr7xehPGRomDEqSb4rcAxyflvI6QBz8GVIVYH5aidn2pSF9jAvkxR8Bv3D33mAHZesSfruXbU3HnThWtdjrZ29RBsXvRrofnIO5iRUnKQCGkBQcTWJikqf8TZKBfnsrpXCohSQH4DY0BCOZCnqeLdwXsQKRfDADJTESmljW8MlyvD3vI3Jm4iEBW98tOLPNUFURmk6ejiCtmgoF6izXNApAcwih68DqyvRqcnc9OQLnQlVcVfrbwp3zJ1oopsN9A7w6hAKOXKNxxDITEm3BShu8ufQaA3zO84SMS52ymliB21EBEuSU7TJB69uwA2mTrAJhgK6I2nThcjH1ZHaTR6peLzUgRYDEW2ENMFIitfOA5ypoAGyFhFn3tuO5wsNmf3vPjDMT488wzOuooQH1894wKL50lSRIvrHv6NrDgiXQrNYSwRvPIQAohhvQcMLDstEYf3GIauM2FQ2ZYhgSXBkQeIpGCsgHvgk2H7A1gzTmpTbUEFxa5EtJwzshkqWB2arjUEjaF8Xn5uqbX63Rih3eJmGxnD6LX2G2Y7vqG8rsNemlPnJnFie3x7hNAwrhJGZWqlevLQb9jrhqwg8kluF7So2ZAGOHUbd5MfWFrHRgTAdE2wbNjcNpBtNAB8lJqKT091kSB0ipP1idA2bPhBk0425xeq8ldsUO8r1")
+        else:
+            self.fail("Should fail for now")
