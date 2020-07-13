b732facf9d410929fc066ca25f208abc031116bd
Alex <axo895@student.bham.ac.uk>
2020-07-13 17:11:18 +0100

    Added code to pass the test

--- CV/tests.py
+++ CV/tests.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest
 from django.test import TestCase
-
+from CV.models import About
 # Create your tests here.
 from CV.views import home_page
 
@@ -11,10 +11,10 @@ class tests(TestCase):
         self.assertTemplateUsed(response, 'CV/base.html')
 
     def test_for_about_model(self):
-        about = About.object.create(description="THis is a test")
+        about = About.objects.create(description="THis is a test")
         about.save()
-        if About.object.get(pk=1):
-
+        if About.objects.get(pk=1):
+            self.assertIsNotNone(About.objects.get(pk=1))
         else:
             self.fail("Didn't create the object")
 
