6efd3605d93b267205f4f148f543d9a0f56c3c3a
Alex <axo895@student.bham.ac.uk>
2020-07-16 19:40:43 +0100

    Added the test for the experience section

--- CV/functional_tests.py
+++ CV/functional_tests.py
@@ -31,6 +31,19 @@ class Tests(unittest.TestCase):
         self.assertIsNotNone(self.browser.find_element_by_id("id_certificate_start_date"))
         self.assertIsNotNone(self.browser.find_element_by_id("id_certificate_end_date"))
 
+    def test_experience_page_cv(self):
+        # The user opens the browser and goes to the experience section of the website
+        self.browser.get('http://127.0.0.1:8000/CV/experience')
+        # The user checks the title of the website
+        self.assertIn('CV', self.browser.title)
+        # The user looks for the content of the experience section
+        self.assertIsNotNone(self.browser.find_element_by_id("id_job_title"))
+        self.assertIsNotNone(self.browser.find_element_by_id("id_job_description"))
+        self.assertIsNotNone(self.browser.find_element_by_id("id_job_company"))
+        self.assertIsNotNone(self.browser.find_element_by_id("id_job_start_date"))
+        self.assertIsNotNone(self.browser.find_element_by_id("id_job_end_date"))
+
+
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
