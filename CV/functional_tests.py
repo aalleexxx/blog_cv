e7f85d382c5251c8498f94ccf964a49d5aaaf078
Alex <axo895@student.bham.ac.uk>
2020-07-16 19:34:10 +0100

    added the test for the education section of the website

--- CV/functional_tests.py
+++ CV/functional_tests.py
@@ -19,6 +19,18 @@ class Tests(unittest.TestCase):
         # The user looks for the about text
         self.assertIsNotNone(self.browser.find_element_by_id("id_about"))
 
+    def test_education_page_cv(self):
+        # The user opens the browser and goes to the education part of the website
+        self.browser.get('http://127.0.0.1:8000/CV/education')
+        # The user checks the title of the website
+        self.assertIn('CV', self.browser.title)
+        # The user looks for the content of the education section
+        self.assertIsNotNone(self.browser.find_element_by_id("id_certificate_title"))
+        self.assertIsNotNone(self.browser.find_element_by_id("id_certificate_description"))
+        self.assertIsNotNone(self.browser.find_element_by_id("id_certificate_institute"))
+        self.assertIsNotNone(self.browser.find_element_by_id("id_certificate_start_date"))
+        self.assertIsNotNone(self.browser.find_element_by_id("id_certificate_end_date"))
+
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
--- CV/views.py
+++ CV/views.py
@@ -1,10 +1,12 @@
 # Create your views here.
 from django.shortcuts import render
 from django.views.generic import ListView
-from CV.models import About
+from CV.models import About, Education
+
 
 class AboutView(ListView):
     model = About
 
     def get_queryset(self):
         return About.objects.all()
+
