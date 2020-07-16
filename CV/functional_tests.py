c8fceef6199b1632b6b00b71fc8df92f95d0b7f5
Alex <axo895@student.bham.ac.uk>
2020-07-16 17:57:07 +0100

    Added the basic test for the main page of the CV

--- CV/functional_tests.py
+++ CV/functional_tests.py
@@ -11,11 +11,13 @@ class Tests(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-
-    # The user enters the CV section of the webpage
     def test_homepage_cv(self):
+        # The user opens the browser and goes to the CV application website
         self.browser.get('http://127.0.0.1:8000/CV')
+        # The user checks the title of the website
         self.assertIn('CV', self.browser.title)
+        # The user looks for the about text
+        self.assertIsNotNone(self.browser.find_element_by_id("id_about"))
 
 
 if __name__ == "__main__":
