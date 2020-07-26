cf9cc8646c558c99a6757f17cc515cd2051c9dd4
Alex <onofrass@gmail.com>
2020-07-26 16:29:36 +0000

    Fixed STATIC_DIR BUG

--- blog_cv/settings.py
+++ blog_cv/settings.py
@@ -114,7 +114,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
-STATICFILES_DIRS = (os.path.join('static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = (os.path.join('static'),)
